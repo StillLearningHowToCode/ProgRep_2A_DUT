a8d18b4cec938b82341f94c5e0ba41ca7a6ad02f
Blue <leanefelis@gmail.com>
2022-03-04 09:29:59 +0100

    maj

--- src/TP3_RMI/Calculateur/Client.java
+++ src/TP3_RMI/Calculateur/Client.java
@@ -11,9 +11,13 @@ public class Client {
             // il faut utiliser la méthode statique lookup() de la classe Naming.
             // Cette méthode attend en paramètre une URL indiquant le nom qui référence l'objet distant.
             CalculateurInterface obj = (CalculateurInterface) Naming.lookup("rmi://localhost:"+ port +"/calculateur");
+            System.out.println("Addition :");
             System.out.println(obj.add(2,5));
+            System.out.println("Soustraction :");
             System.out.println(obj.sub(2,5));
+            System.out.println("Multiplication :");
             System.out.println(obj.mul(2,5));
+            System.out.println("Division :");
             System.out.println(obj.div(2,5));
         } catch (Exception e) {
             System.out.println("Calculateur Client exception: " + e);
